444e545b42b322961f3bae19cc8f33426a681624
VictorFilgueirasBR <app.se.manager@gmail.com>
2025-09-16 23:12:52 -0300

    VideoWelcome

---
+++ client/src/components/VideoWelcome.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+export default function VideoPlayer() {
+  const css = `
+    .video-player-container {
+      width: 100%;
+      height: 100%;
+      margin-top: 60px;
+      position: relative;
+      overflow: hidden;
+      background: #FFF;
+    }
+
+    .responsive-video {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+
+    @media (orientation: portrait) {
+
+    .video-player-container {
+      width: 100%;
+      height: 250px;
+      position: relative;
+      overflow: hidden;
+    }
+      .responsive-video {
+        width: 100%;
+        height: 100%;
+        transform: rotate(0deg);
+      }
+    }
+  `;
+
+  return (
+    <div className="video-player-container">
+      <style>{css}</style>
+      <video className="responsive-video" autoPlay muted loop playsInline>
+        <source src="/reels/welcomehomewsc.mp4" type="video/mp4" />
+        Seu navegador não suporta a tag de vídeo.
+      </video>
+    </div>
+  );
+}
