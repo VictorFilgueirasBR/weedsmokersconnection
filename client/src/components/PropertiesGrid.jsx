9dc105d7319045ea2726c66898ab89b4ab94ba52
VictorFilgueirasBR <app.se.manager@gmail.com>
2025-09-18 15:53:50 -0300

    Link plug

--- client/src/components/PropertiesGrid.jsx
+++ client/src/components/PropertiesGrid.jsx
@@ -4,61 +4,66 @@ import './PropertiesGrid.scss';
 
 // Dados dos imóveis com as imagens fornecidas e informações fictícias
 const propertiesData = [
-  { 
-    id: 1, 
-    image: '/images/hash-legalizado-anvisa.jpg', 
-    title: 'Hash ICE (THC)', 
-    location: '5g', 
-    price: 'R$ 350,00', 
-    description: 'Fornecedor Nacional de Hash e EXtrações tipo ICE legalizado pela ANVISA.', 
-    cta: 'Contato' 
+  {
+    id: 1,
+    image: '/images/hash-legalizado-anvisa.jpg',
+    title: 'Hash ICE (THC)',
+    location: '5g',
+    price: 'R$ 350,00',
+    description: 'Fornecedor Nacional de Hash e Extrações tipo ICE legalizado pela ANVISA.',
+    cta: 'Contato',
+    link: 'http://instagram.com/Greenway.cbd'
   },
-  { 
-    id: 2, 
-    image: '/images/beautiful-buds.jpg', 
-    title: 'Flores HK (THC)', 
-    location: '5g', 
-    price: 'R$ 195,00', 
-    description: 'Fornecedor Nacional de Flores em Natura com THC legalizado pela ANVISA.', 
-    cta: 'Ver Detalhes' 
+  {
+    id: 2,
+    image: '/images/beautiful-buds.jpg',
+    title: 'Flores HK (THC)',
+    location: '5g',
+    price: 'R$ 195,00',
+    description: 'Fornecedor Nacional de Flores em Natura com THC legalizado pela ANVISA.',
+    cta: 'Contato',
+    link: 'https://institutozasso.com.br/'
   },
-  { 
-    id: 3, 
-    image: '/images/club-flores.jpeg', 
-    title: 'Flores Z-Kittlez Pie (THC)', 
-    location: '5g', 
-    price: 'R$ 220,00', 
-    description: 'Fornecedor Nacional de Flores em Natura com THC legalizado pela ANVISA.', 
-    cta: 'Contato' 
+  {
+    id: 3,
+    image: '/images/club-flores.jpeg',
+    title: 'Flores Z-Kittlez Pie (THC)',
+    location: '5g',
+    price: 'R$ 220,00',
+    description: 'Fornecedor Nacional de Flores em Natura com THC legalizado pela ANVISA.',
+    cta: 'Contato',
+    link: 'https://abecmed.com.br/?fbclid=PAT01DUAMvMpBleHRuA2FlbQIxMAABp3Nsla7Cr-Nm9W3qCd0iiTHb1TyuLtjx3ved4ZcTiDqfSIZ0wlzeR7DVTQ9g_aem_rbZtUFtYN_y2wZun7tsCGQ'
   },
-  { 
-    id: 4, 
-    image: '/images/hash-legalizado-anvisa-capa.jpg', 
-    title: 'Hash ICE - 24K (THC)', 
-    location: '10g', 
-    price: 'R$ 600,00', 
-    description: 'Fornecedor Importado (EU) de Extrações tipo ICE com THC legalizado pela ANVISA.', 
-    cta: 'Contato' 
+  {
+    id: 4,
+    image: '/images/hash-legalizado-anvisa-capa.jpg',
+    title: 'Hash ICE - 24K (THC)',
+    location: '10g',
+    price: 'R$ 600,00',
+    description: 'Fornecedor Nacional de Extrações tipo ICE com THC legalizado pela ANVISA.',
+    cta: 'Contato',
+    link: 'http://instagram.com/Greenway.cbd'
   },
-  { 
-    id: 5, 
-    image: '/images/wsc-vape-thc.webp', 
-    title: 'Refil THC puro', 
-    location: 'Unidade.', 
-    price: 'R$ 220,00', 
-    description: 'Fornecedor Importado (EU) de Extrações com THC legalizado pela ANVISA.', 
-    cta: 'Contato' 
+  {
+    id: 5,
+    image: '/images/wsc-vape-thc.webp',
+    title: 'Refil THC puro',
+    location: 'Unidade.',
+    price: 'R$ 220,00',
+    description: 'Fornecedor Importado (EU) de Extrações com THC legalizado pela ANVISA.',
+    cta: 'Contato',
+    link: 'https://flowermed.com.br/'
   },
-  { 
-    id: 6, 
-    image: '/images/hash-ice-import.png', 
-    title: 'ICE Bubble Hash (THC)', 
-    location: '10g', 
-    price: 'R$ 650,00', 
-    description: 'Fornecedor Importado (EU) de Extrações tipo ICE com THC legalizado pela ANVISA.', 
-    cta: 'Contato' 
+  {
+    id: 6,
+    image: '/images/hash-ice-import.png',
+    title: 'ICE Bubble Hash (THC)',
+    location: '10g',
+    price: 'R$ 650,00',
+    description: 'Fornecedor Importado (EU) de Extrações tipo ICE com THC legalizado pela ANVISA.',
+    cta: 'Contato',
+    link: 'https://flowermed.com.br/'
   },
-  
 ];
 
 const PropertiesGrid = ({ title = 'Fornecedores Legalmente Autorizados pela ANVISA', id = 'properties-grid' }) => {
@@ -76,7 +81,7 @@ const PropertiesGrid = ({ title = 'Fornecedores Legalmente Autorizados pela ANVI
               <h3 className="property-title">{property.title}</h3>
               <p className="property-location">{property.location}</p>
               <p className="property-description">{property.description}</p>
-              <button className="property-cta submit-gradient-btn">{property.cta}</button>
+              <a href={property.link} target="_blank" rel="noopener noreferrer" className="property-cta submit-gradient-btn">{property.cta}</a>
             </div>
           </div>
         ))}
