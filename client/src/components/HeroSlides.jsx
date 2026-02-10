d97a68aac054d35a157832ccbcdfcb383872bbb9
VictorFilgueirasBR <app.se.manager@gmail.com>
2026-02-10 19:03:06 -0300

    News

--- client/src/components/HeroSlides.jsx
+++ client/src/components/HeroSlides.jsx
@@ -83,7 +83,7 @@ const HeroSlides = () => {
         onMouseMove={handleMouseMove}
         onClick={() => navigate('/signup')}
       >
-        ASSINAR →
+        CONTRATAR →
       </button>
 
       {/* Conteúdo fixo sobre o slider, com estilo glassmorphism */}
