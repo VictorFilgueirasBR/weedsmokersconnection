7b6aea048ed81e7a22554d5b4a54ad32d44929d0
VictorFilgueirasBR <app.se.manager@gmail.com>
2026-02-13 19:32:05 -0300

    New herohome

--- client/src/components/HeroSlides.jsx
+++ client/src/components/HeroSlides.jsx
@@ -89,7 +89,7 @@ const HeroSlides = () => {
       {/* Conteúdo fixo sobre o slider, com estilo glassmorphism */}
       <div className="hero-content-overlay">
         <p className="hero-subtitle">
-          Entre para o CLUB, siga o passo a passo para ter autorização e escolha seus Fornecedores das Melhores Espécies e Extrações Ricas em THC e CBD, Nacionais e Importadas de forma LEGALIZADA pela ANVISA aqui no BRASIL.
+          Escolha um Plano de Contratação, siga o passo a passo para ter autorização e escolha seus Fornecedores das Melhores Espécies e Extrações Ricas em THC e CBD, Nacionais e Importadas de forma LEGALIZADA pela ANVISA aqui no BRASIL.
         </p>
       </div>
     </section>
