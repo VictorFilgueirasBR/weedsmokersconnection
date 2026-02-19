d9d1b06340f96579359611607dcfffa44b0c8786
VictorFilgueirasBR <app.se.manager@gmail.com>
2026-02-18 22:10:59 -0300

    New btn suport

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -550,10 +550,10 @@ export default function Signup() {
                 <button
                     type="button"
                     className="submit-gradient-btn"
-                    onClick={() => window.open("https://www.instagram.com/wsconnectionbrasil/", "_blank")}
+                    onClick={() => window.open("https://wa.me/5561995276936", "_blank")}
                     onMouseMove={handleMouseMove}
                 >
-                    ATENDIMENTOS VIA DM
+                    WHATSAPP
                 </button>
 
 
