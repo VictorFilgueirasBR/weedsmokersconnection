8e25cee85eded279559b0f35e1a46b76b54053d0
VictorFilgueirasBR <app.se.manager@gmail.com>
2026-04-19 21:09:24 -0300

    New order

--- client/src/components/HeroSlides.jsx
+++ client/src/components/HeroSlides.jsx
@@ -83,7 +83,7 @@ const HeroSlides = () => {
         onMouseMove={handleMouseMove}
         onClick={() => navigate('/signup')}
       >
-        ENTRAR NO CLUB →
+        CONTRATAR →
       </button>
 
       {/* Conteúdo fixo sobre o slider, com estilo glassmorphism */}
