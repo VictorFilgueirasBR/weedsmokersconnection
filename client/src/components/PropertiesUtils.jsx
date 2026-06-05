64afa3ce03b8b4a7e28a277d61700b2639319208
VictorFilgueirasBR <app.se.manager@gmail.com>
2026-06-05 14:30:07 -0300

    Pow

--- client/src/components/PropertiesUtils.jsx
+++ client/src/components/PropertiesUtils.jsx
@@ -10,7 +10,7 @@ const propertiesUtilsData = [
     title: 'WSC Consulta | ANVISA',
     location: 'R$ 400,00',
     price: 'WSC-COMPLETE',
-    description: 'Renovação 6 meses para pacientes: Consulta médica + emissão de documentação pela ANVISA para importação legal.',
+    description: 'Renovação 6 meses para pacientes:       Consulta médica + emissão de documentação pela ANVISA para importação legal.',
     cta: 'Contratar',
     link: 'https://ws-connectioncommerce.com/produto/702/'
   },
